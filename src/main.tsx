18daefd2c3db1293b92bd72583a2d2064b95827e
BStation1 <ahmed.ellbanna@gmail.com>
2026-08-15 03:52:05 +0300

    fix: install shared username login resolver

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './lib/installDeliveredOrderHardDeleteButton';
+import './lib/installUsernameLogin';
 import { bootstrapAuthoritativeSync } from './lib/bootstrapAuthoritativeSync';
 
 async function bootstrap() {
