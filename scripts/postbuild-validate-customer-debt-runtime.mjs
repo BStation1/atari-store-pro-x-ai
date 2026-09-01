e038b18f2c7f7267a2635b6ca93a1dd5aea091d8
BStation1 <ahmed.ellbanna@gmail.com>
2026-09-01 14:31:16 +0300

    Remove temporary smoke check

--- scripts/postbuild-validate-customer-debt-runtime.mjs
+++
@@ -1,5 +0,0 @@
-import fs from 'node:fs';
-const src = fs.readFileSync('src/components/CustomersList.tsx','utf8');
-if (!src.includes('function getCustomerOutstandingBalance')) throw new Error('Debt function must be hoisted');
-if (src.includes('const getCustomerOutstandingBalance =')) throw new Error('TDZ-prone debt const still present');
-console.log('Customer debt runtime smoke check passed');
